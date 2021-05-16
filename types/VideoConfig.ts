1d9f298005bb3f34cfd5ca7a23c97c2c461faec2
Bravo13 <bravo13blog@gmail.com>
2021-05-16 19:32:57 +0300

    add type for video settings

--- types/VideoConfig.ts
+++ types/VideoConfig.ts
@@ -1,5 +1,11 @@
-export type VideoConfig = String | {
+import { TextOverlayConfig } from "./TextOverlayConfig";
+
+export type VideoConfig = {
     path: string,
     begin?: string,
-    end?: string
+    end?: string,
+    beginOffset?: number,
+    endOffset?: number,
+    subtitle?: TextOverlayConfig,
+    text?: string
 }
